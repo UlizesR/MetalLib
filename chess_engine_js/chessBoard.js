d5c4a56d8b0236ea37aadb3baa66ce15decce61b
Uli Rodriguez <fulizesraudales@gmail.com>
2023-07-03 13:40:35 -0700

    updated chessboard position logic

--- chess_engine_js/chessBoard.js
+++ chess_engine_js/chessBoard.js
@@ -1,117 +1,121 @@
 
 
 class ChessBoard {
-    constructor() {
-        // 0 = white, 1 = black
-        this.board = [
-            ["B_R", "B_N", "B_B", "B_Q", "B_K", "B_B", "B_N", "B_R"],
-            ["B_P", "B_P", "B_P", "B_P", "B_P", "B_P", "B_P", "B_P"],
-            [null, null, null, null, null, null, null, null],
-            [null, null, null, null, null, null, null, null],
-            [null, null, null, null, null, null, null, null],
-            [null, null, null, null, null, null, null, null],
-            ["W_P", "W_P", "W_P", "W_P", "W_P", "W_P", "W_P", "W_P"],
-            ["W_R", "W_N", "W_B", "W_Q", "W_K", "W_B", "W_N", "W_R"]
-        ];
-        this.enPassantTarget = null;
-    }
-
-    /**
-     * Takes the from and to Position of the form 'colrow' and moves the piece at the from position to the to position 
-     * @param {string} position
-     * @throws {Error} No piece at position
-     * @throws {Error} Illegal move
-     */
-
-    movePiece(from, to) {
-        const piece = this.getPiece(from);
-    
-        if (!piece) {
-          throw new Error("No piece at position");
-        }
-    
-        const legalMoves = piece.legalMoves();
-    
-        if (!legalMoves.some(move => move[0] === to[0] && move[1] === to[1])) {
-          throw new Error("Illegal move");
-        }
-    
-        // Check for en passant capture
-        if (piece instanceof Pawn && Math.abs(from[1] - to[1]) === 2) {
-          this.enPassantTarget = [(from[0] + to[0]) / 2, from[1]];
-        } else {
-          this.enPassantTarget = null;
-        }
-    
-        // Move the piece
-        this.setPiece(piece, to);
-        this.deletePiece(from);
-    
-        // Check for pawn promotion
-        if (piece instanceof Pawn && (to[1] === 0 || to[1] === 7)) {
-          this.setPiece(new Queen(piece.getColor(), to), to);
-        }
+  constructor() {
+      this.board = {
+        "a8": "B_R", "b8": "B_N", "c8": "B_B", "d8": "B_Q", "e8": "B_K", "f8": "B_B", "g8": "B_N", "h8": "B_R",
+        "a7": "B_P", "b7": "B_P", "c7": "B_P", "d7": "B_P", "e7": "B_P", "f7": "B_P", "g7": "B_P", "h7": "B_P",
+        "a6": null, "b6": null, "c6": null, "d6": null, "e6": null, "f6": null, "g6": null, "h6": null,
+        "a5": null, "b5": null, "c5": null, "d5": null, "e5": null, "f5": null, "g5": null, "h5": null,
+        "a4": null, "b4": null, "c4": null, "d4": null, "e4": null, "f4": null, "g4": null, "h4": null,
+        "a3": null, "b3": null, "c3": null, "d3": null, "e3": null, "f3": null, "g3": null, "h3": null,
+        "a2": "W_P", "b2": "W_P", "c2": "W_P", "d2": "W_P", "e2": "W_P", "f2": "W_P", "g2": "W_P", "h2": "W_P",
+        "a1": "W_R", "b1": "W_N", "c1": "W_B", "d1": "W_Q", "e1": "W_K", "f1": "W_B", "g1": "W_N", "h1": "W_R"
       }
+      this.positions = [
+          "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
+          "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
+          "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
+          "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
+          "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
+          "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
+          "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
+          "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"
+      ]
+      this.enPassantTarget = null;
+  }
 
-    /**
-     * Takes Position of the form 'colrow' and returns the piece at that position
-     * @param {string} position 
-     * @returns {string}
-     */
-    getPiece(position) {
-        const col = position.charCodeAt(0) - 97; // Convert column character to numeric index
-        const row = position[1] - 1; // Convert row character to numeric index
-
-        if (!(row >= 0 && row < 8 && col >= 0 && col < 8)) {
-            throw new Error("Invalid position"); // Throw an exception for invalid positions
-        }
-
-        // will return null if position is empty or will return the piece value
-        return this.board[row][col];
+  /**
+   * Takes the from and to Position of the form 'colrow' and moves the piece at the from position to the to position 
+   * @param  {string} position
+   * @throws {Error} No piece at position
+   * @throws {Error} Illegal move
+   */
+  movePiece(from, to) {
+    // check if there is a piece at the to position
+    if (this.board[to] && this.board[from][0] === this.board[to][0]) {
+      throw new Error("Illegal move");
     }
 
-    /**
-     * Takes Position of the form 'colrow' and sets the piece at that position
-     * @param {Piece} piece 
-     * @param {Array} position 
-     */
-    setPiece(piece_abr, position) {
-        const col = position.charCodeAt(0) - 97; // Convert column character to numeric index
-        const row = position[1] - 1; // Convert row character to numeric index
+    // move piece
+    this.setPiece(this.getPiece(from), to);
+    this.deletePiece(from);
 
-        if (!(row >= 0 && row < 8 && col >= 0 && col < 8)) {
-            throw new Error("Invalid position"); // Throw an exception for invalid positions
-        }
-
-        // set piece to board
-        this.board[row][col] = piece_abr;
-    }
-
-    /**
-     * Takes Position of the form [col, row] and deletes the piece at that position
-     * @param {Array} position
-     * @returns {string}
-     * @throws {Error} Invalid position
-     * @throws {Error} No piece at position
-    */
-    deletePiece(position) {
-        const col = position.charCodeAt(0) - 97; // Convert column character to numeric index
-        const row = position[1] - 1; // Convert row character to numeric index
-
-        if (!(row >= 0 && row < 8 && col >= 0 && col < 8)) {
-            throw new Error("Invalid position"); // Throw an exception for invalid positions
-        }
-        if (!(this.board[row][col])) {
-            throw new Error("No piece at position"); // Throw an exception for invalid positions
-        }
-        // delete piece from board and sets to null
-        this.board[row][col] = null;
-    }
-  
-    printBoard() {
-      for (let i = 0; i < this.board.length; i++) {
-        console.log(this.board[i]);
-      }
-    }
   }
   
+  /**
+   * Takes Position of the form 'colrow' and returns the piece at that position
+   * @param   {string} position 
+   * @throws  {Error} Invalid position
+   * @returns {string} piece_abr or null
+   */
+  getPiece(position) {
+    // check if position is valid
+    if (!this.positions.includes(position)) {
+      throw new Error(`Invalid position: ${position}`);
+    }
+    // will return null if position is empty or will return the piece value
+    return this.board[position] || null;
+  }
+
+  /**
+   * Takes Position of the form 'colrow' and sets the piece at that position
+   * @param {Piece} piece 
+   * @param {Array} position 
+   * @throws {Error} Invalid position
+   */
+  setPiece(piece_abr, position) {
+      // check if position is valid
+      if (!this.positions.includes(position)) {
+        throw new Error(`Invalid position: ${position}`);
+      }
+      // set piece to board
+      this.board[position] = piece_abr;
+  }
+
+  /**
+   * Takes Position of the form [col, row] and deletes the piece at that position
+   * @param   {Array} position
+   * @returns {string}
+   * @throws  {Error} Invalid position
+   * @throws  {Error} No piece at position
+  */
+  deletePiece(position) {
+      // check if position is valid
+      if (!this.positions.includes(position)) {
+        throw new Error(`Invalid position: ${position}`);
+      }
+      if (!(this.board[position])) {
+          throw new Error("No piece at position"); 
+      }
+      // delete piece from board and sets to null
+      this.board[position] = null;
+  }
+  
+  printBoard() {
+    const rows = ['8', '7', '6', '5', '4', '3', '2', '1'];
+    const cols = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+    const squareWidth = 3;
+    let boardStr = '';
+  
+    for (let row of rows) {
+      boardStr += row + ' ';
+      for (let col of cols) {
+        const piece = this.getPiece(col + row);
+        const square = piece ? piece : ' . ';
+        const padding = ' '.repeat(squareWidth - square.length);
+        boardStr += square + ' ' + padding;
+      }
+      boardStr += '\n';
+    }
+  
+    boardStr += '   ' + cols.join(' '.repeat(squareWidth)) + '\n';
+    console.log(boardStr);
+  }
+}
+
+module.exports = ChessBoard;
+
+// call to test
+const board = new ChessBoard();
+board.printBoard();
--- chess_engine_js/pieces.js
+++ chess_engine_js/pieces.js
@@ -37,15 +37,60 @@ class Pawn extends Piece {
 
     legalMoves() {
         let moves = [];
-        let col = this.position.charCodeAt(0) - 97;
+        let col = this.position[0];
+        let col_num = this.position.charCodeAt(0) - 97;
         let row = this.position[1];
 
         if (this.color === 'white') {
-            if (row === '2') {
-                moves.push([col, row + 2]);
+            // check for pawn at starting position (row 2) and no pieces in front of it
+            // if so, add the position two spaces in front of the pawn to the moves array
+            if (row === 2 && !this.board.getPiece(`${col}${row + 1}`) && !this.board.getPiece(`${col}${row + 2}`)) {
+                moves.push(`${col}${row + 2}`);
             }
-            moves.push([col, row + 1]);
-        } else {}
+            // check if there is a piece in front of the pawn
+            // if not, add the position to the moves array
+            if (!this.board.getPiece(`${col}${row + 1}`)) {
+                moves.push(`${col}${row + 1}`);
+            }
+            // check if there is an opponent piece diagonally to the right of the pawn
+            // if so, add the position to the moves array
+            if (this.board.getPiece(`${String.fromCharCode(col_num + 1)}${row + 1}`) && 
+                this.board.getPiece(`${String.fromCharCode(col_num + 1)}${row + 1}`)[0] === 'B') {
+                moves.push(`${String.fromCharCode(col_num + 1)}${row + 1}`);
+            }
+            // check if there is an opponent piece diagonally to the left of the pawn
+            // if so, add the position to the moves array
+            if (this.board.getPiece(`${String.fromCharCode(col_num - 1)}${row + 1}`) && 
+                this.board.getPiece(`${String.fromCharCode(col_num - 1)}${row + 1}`)[0] === 'B') {
+                moves.push(`${String.fromCharCode(col_num - 1)}${row + 1}`);
+            }
+            // check if there is an opponent piece en passant to the right of the pawn
+            // if so, add the position to the moves array
+
+
+            moves.push(`${col}${row + 1}`);
+        } else {
+            if (row === 7) {
+                moves.push(`${col}${row - 2}`);
+            }
+            // check if there is a piece in front of the pawn
+            // if not, add the position to the moves array
+            if (!this.board.getPiece(`${col}${row - 1}`)) {
+                moves.push(`${col}${row - 1}`);
+            }
+            // check if there is an opponent piece diagonally to the right of the pawn
+            // if so, add the position to the moves array
+            if (this.board.getPiece(`${String.fromCharCode(col_num + 1)}${row - 1}`) &&
+                this.board.getPiece(`${String.fromCharCode(col_num + 1)}${row - 1}`)[0] === 'W') {
+                moves.push(`${String.fromCharCode(col_num + 1)}${row - 1}`);
+            }
+            // check if there is an opponent piece diagonally to the left of the pawn   
+            // if so, add the position to the moves array
+            if (this.board.getPiece(`${String.fromCharCode(col_num - 1)}${row - 1}`) &&
+                this.board.getPiece(`${String.fromCharCode(col_num - 1)}${row - 1}`)[0] === 'W') {
+                moves.push(`${String.fromCharCode(col_num - 1)}${row - 1}`);
+            }
+        }
         
         return moves;
     }
@@ -62,7 +107,102 @@ class Rook extends Piece {
     }
 
     legalMoves() {
+        let moves = [];
+        let col = this.position[0];
+        let col_num = this.position.charCodeAt(0) - 97;
+        let row_num = parseInt(this.position[1]);
 
+        if (this.color === 'white') {
+            // check for moves to the right
+            for (let i = col_num + 1; i < 8; i++) {
+                if (this.board.getPiece(`${String.fromCharCode(i)}${row_num}`)) {
+                    if (this.board.getPiece(`${String.fromCharCode(i)}${row_num}`)[0] === 'B') {
+                        moves.push(`${String.fromCharCode(i)}${row_num}`);
+                    }
+                    break;
+                }
+                moves.push(`${String.fromCharCode(i)}${row_num}`);
+            }
+
+            // check for moves to the left
+            for (let i = col_num - 1; i >= 0; i--) {
+                if (this.board.getPiece(`${String.fromCharCode(i)}${row_num}`)) {
+                    if (this.board.getPiece(`${String.fromCharCode(i)}${row_num}`)[0] === 'B') {
+                        moves.push(`${String.fromCharCode(i)}${row_num}`);
+                    }
+                    break;
+                }
+                moves.push(`${String.fromCharCode(i)}${row_num}`);
+            }
+
+            // check for moves to the top
+            for (let i = row_num + 1; i < 9; i++) {
+                if (this.board.getPiece(`${col}${i}`)) {
+                    if (this.board.getPiece(`${col}${i}`)[0] === 'B') {
+                        moves.push(`${col}${i}`);
+                    }
+                    break;
+                }
+                moves.push(`${col}${i}`);
+            }
+
+            // check for moves to the bottom
+            for (let i = row_num - 1; i >= 0; i--) {
+                if (this.board.getPiece(`${col}${i}`)) {
+                    if (this.board.getPiece(`${col}${i}`)[0] === 'B') {
+                        moves.push(`${col}${i}`);
+                    }
+                    break;
+                }
+                moves.push(`${col}${i}`);
+            }
+        } else {
+            // check for moves to the right
+            for (let i = col_num + 1; i < 8; i++) {
+                if (this.board.getPiece(`${String.fromCharCode(i)}${row_num}`)) {
+                    if (this.board.getPiece(`${String.fromCharCode(i)}${row_num}`)[0] === 'W') {
+                        moves.push(`${String.fromCharCode(i)}${row_num}`);
+                    }
+                    break;
+                }
+                moves.push(`${String.fromCharCode(i)}${row_num}`);
+            }
+
+            // check for moves to the left
+            for (let i = col_num - 1; i >= 0; i--) {
+                if (this.board.getPiece(`${String.fromCharCode(i)}${row_num}`)) {
+                    if (this.board.getPiece(`${String.fromCharCode(i)}${row_num}`)[0] === 'W') {
+                        moves.push(`${String.fromCharCode(i)}${row_num}`);
+                    }
+                    break;
+                }
+                moves.push(`${String.fromCharCode(i)}${row_num}`);
+            }
+
+            // check for moves to the top
+            for (let i = row_num + 1; i < 9; i++) {
+                if (this.board.getPiece(`${col}${i}`)) {
+                    if (this.board.getPiece(`${col}${i}`)[0] === 'W') {
+                        moves.push(`${col}${i}`);
+                    }
+                    break;
+                }
+                moves.push(`${col}${i}`);
+            }
+
+            // check for moves to the bottom
+            for (let i = row_num - 1; i >= 0; i--) {
+                if (this.board.getPiece(`${col}${i}`)) {
+                    if (this.board.getPiece(`${col}${i}`)[0] === 'W') {
+                        moves.push(`${col}${i}`);
+                    }
+                    break;
+                }
+                moves.push(`${col}${i}`);
+            }
+        }  
+
+        return moves;
     }
 }
 
@@ -77,7 +217,54 @@ class Knight extends Piece {
     }
 
     legalMoves() {
+        let moves = [];
+        let col = this.position[0];
+        let col_num = this.position.charCodeAt(0) - 97;
+        let row_num = parseInt(this.position[1]);
 
+        let upsideDownL_R = this.board.getPiece(`${String.fromCharCode(col_num + 1)}${row_num + 2}`) // 2 up 1 right
+        let upsideDownL_L = this.board.getPiece(`${String.fromCharCode(col_num - 1)}${row_num + 2}`) // 2 up 1 left
+        let flatL_U_R     = this.board.getPiece(`${String.fromCharCode(col_num + 2)}${row_num + 1}`) // 1 up 2 right
+        let flatL_U_L     = this.board.getPiece(`${String.fromCharCode(col_num - 2)}${row_num + 1}`) // 1 up 2 left
+        let flatL_D_R     = this.board.getPiece(`${String.fromCharCode(col_num + 2)}${row_num - 1}`) // 1 down 2 right
+        let flatL_D_L     = this.board.getPiece(`${String.fromCharCode(col_num - 2)}${row_num - 1}`) // 1 down 2 left
+        let regularL_R    = this.board.getPiece(`${String.fromCharCode(col_num + 1)}${row_num - 2}`) // 2 down 1 right
+        let regularL_L    = this.board.getPiece(`${String.fromCharCode(col_num - 1)}${row_num - 2}`) // 2 down 1 left
+
+
+
+        if (this.color === 'white') {
+            // check for moves to the top
+            if ((!upsideDownL_R || upsideDownL_R[0] === 'B') && upsideDownL_R !== "Invalid Position") {
+                moves.push(`${String.fromCharCode(col_num + 1)}${row_num + 2}`);
+            }
+            if ((!upsideDownL_L || upsideDownL_L[0] === 'B') && upsideDownL_L !== "Invalid Position") {
+                moves.push(`${String.fromCharCode(col_num - 1)}${row_num + 2}`);
+            }
+            if ((!flatL_U_R || flatL_U_R[0] === 'B') && flatL_U_R !== "Invalid Position") {
+                moves.push(`${String.fromCharCode(col_num + 2)}${row_num + 1}`);
+            }
+            if ((!flatL_U_L || flatL_U_L[0] === 'B') && flatL_U_L !== "Invalid Position") {
+                moves.push(`${String.fromCharCode(col_num - 2)}${row_num + 1}`);
+            }
+            if ((!flatL_D_R || flatL_D_R[0] === 'B') && flatL_D_R !== "Invalid Position") {
+                moves.push(`${String.fromCharCode(col_num + 2)}${row_num - 1}`);
+            }
+            if ((!flatL_D_L || flatL_D_L[0] === 'B') && flatL_D_L !== "Invalid Position") {
+                moves.push(`${String.fromCharCode(col_num - 2)}${row_num - 1}`);
+            }
+            if ((!regularL_R || regularL_R[0] === 'B') && regularL_R !== "Invalid Position") {
+                moves.push(`${String.fromCharCode(col_num + 1)}${row_num - 2}`);
+            }
+            if ((!regularL_L || regularL_L[0] === 'B') && regularL_L !== "Invalid Position") {
+                moves.push(`${String.fromCharCode(col_num - 1)}${row_num - 2}`);
+            }
+            
+        } else {
+
+        }
+
+        return moves;
     }
 }
 
