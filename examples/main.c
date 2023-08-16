d2cc47184ded05c9027918f6028eb55347a03f75
Uli Rodriguez <fulizesrp@gmail.com>
2023-08-16 01:11:47 -0700

    set display device

--- examples/main.c
+++ examples/main.c
@@ -1,4 +1,5 @@
 #include "MIA/m_delegate.h"
+#include "MIA/m_devices.h"
 #include <MIA/m_window.h>
 #include <MIA/m_init.h>
 
@@ -13,20 +14,23 @@ int main()
         return 1;
     }
 
-    M_Window* window = M_CreateWindow(800, 600, true, "MIA Window", M_WINDOW_RESIZABLE | M_WINDOW_MINIMIZED);
-    if(window == NULL)
-    {
-        fprintf(stderr, "Error: M_CreateWindow() failed\n");
-        M_Quit();
-        return 1;
-    }
+    // M_Window* window = M_CreateWindow(800, 600, true, "MIA Window", M_WINDOW_RESIZABLE | M_WINDOW_MINIMIZED);
+    // if(window == NULL)
+    // {
+    //     fprintf(stderr, "Error: M_CreateWindow() failed\n");
+    //     M_Quit();
+    //     return 1;
+    // }
 
-    while(M_IsWindowOpen(window))
-    {
-        runDelegate();
-    }
+    M_Display display = M_GetDisplay(0);
+    M_PrintDisplayData(display);
 
-    M_DestroyWindow(window);
+    // while(M_IsWindowOpen(window))
+    // {
+    //     runDelegate();
+    // }
+
+    // M_DestroyWindow(window);
     M_Quit();
 
     return 0;
