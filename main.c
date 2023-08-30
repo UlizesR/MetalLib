4c1af2d2217722346edf30b5a4d254981a4be680
Uli Raudales <fulizesrp@gmail.com>
2023-08-29 21:31:51 -0700

    updated some stuff

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <MCL/MCL.h>
+#include <MCL/colors.h>
 #include <stdbool.h>
 #include <stdio.h>
 
@@ -16,12 +17,12 @@ int main() {
     M_Quit();
     return 1;
   }
-  M_AddContentView(window, M_COLOR_CYAN_8);
+  M_AddContentView(window, M_COLOR_BLACK);
 
-  M_Point *point = M_CreatePoint(100, 600, M_COLOR_BLACK);
+  M_Point *point = M_CreatePoint(100, 600, M_COLOR_WHITE);
   M_DrawPoint(window->content_view, point);
 
-  M_Line *line = M_CreateLine((MFPoint){100, 100}, (MFPoint){200, 100},  1, M_COLOR_BLACK);
+  M_Line *line = M_CreateLine((MFPoint){100, 100}, (MFPoint){200, 100},  1, M_COLOR_WHITE);
   M_DrawLine(window->content_view, line);
 
   while (M_IsWindowOpen(window)) {
